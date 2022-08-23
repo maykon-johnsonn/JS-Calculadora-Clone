05eb4d8cecb854b680b1d163a117f7df985ce691
Maykon Johnsonn <maykonjohnsonn@hotmail.com>
2022-08-22 23:35:03 -0300

    Criado getters e setters, criado initialize

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -1,3 +1,37 @@
 class CalcController {
     
+    constructor(){
+        this._displayCalc = "0";
+        this._currentDate;
+        this.initialize();
+    }
+
+    initialize(){
+        let displayCalcEl = document.querySelector("#display");
+        let dateEl = document.querySelector("#data");
+        let timeEl = document.querySelector("#hora");
+
+        displayCalcEl.innerHTML = "4567";
+        dateEl.innerHTML = "22/08/22";
+        timeEl.innerHTML = "22:25"
+    }
+
+    //Recupera o valor presente em displayCalc
+    get displayCalc(){
+        return this._displayCalc;
+    }
+    //Insere um novo valor para displayCalc
+    set displayCalc(value){
+        this._displayCalc = value;
+    }
+    // -------------------- 
+    get dataAtual(){
+        return this._currentDate;
+    }
+
+    set dataAtual(value){
+        this._currentDate = value;
+    }
+
+
 }
