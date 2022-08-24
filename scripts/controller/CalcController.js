08a4d06ba93f963c2018ccf0c97699e4ba927d38
Maykon Johnsonn <maykonjohnsonn@hotmail.com>
2022-08-24 11:49:49 -0300

    Criado eventos aos botões, inclusão do pointer(efeito) ao cursor

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -7,6 +7,8 @@ class CalcController {
         this._timeEl = document.querySelector("#hora");
         this._currentDate;
         this.initialize();
+        this.initButtonsEvent();
+        //this.addEventListenerAll();
     }
 
     initialize(){
@@ -16,6 +18,27 @@ class CalcController {
         setInterval(()=>{
             this.setDisplayDateTime();
         }, 1000);
+    }
+
+    addEventListenerAll(element, events, fn){
+        events.split(' ').forEach(event =>{
+            element.addEventListener(event, fn, false);
+        });
+    }
+
+    initButtonsEvent(){
+        let buttons = document.querySelectorAll("#buttons > g, #parts > g");
+
+        buttons.forEach((btn, index)=>{
+            this.addEventListenerAll(btn, "click drag", e=>{
+                console.log(btn.className.baseVal.replace("btn-", ""));
+            });
+            this.addEventListenerAll(btn, "mouseover mouseup mousedown", e => {
+
+                btn.style.cursor = "pointer";
+
+            })
+        })
     }
 
     setDisplayDateTime(){
@@ -56,6 +79,4 @@ class CalcController {
     set currentDate(value){
         this._currentDate = value;
     }
-
-
 }
